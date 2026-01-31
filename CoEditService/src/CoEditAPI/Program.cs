facd6716434bc054f349d36df01f17be8d9d941a
Thilakshan Baskaran <telakshanb@gmail.com>
2026-01-31 09:57:19 -0500

    refactor: standardize solution file paths and simplify Kestrel server configuration.

--- CoEditService/src/CoEditAPI/Program.cs
+++ CoEditService/src/CoEditAPI/Program.cs
@@ -1,16 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.WebHost.ConfigureKestrel((context, options) =>
-{
-    options.ListenAnyIP(5000);
-    options.ListenAnyIP(5001, listenOptions =>
-    {
-        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
-        listenOptions.UseHttps();
-    });
-});
 
 
 builder.Services.AddControllers();
